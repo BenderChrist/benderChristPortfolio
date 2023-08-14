f3e98dd03acb32fa2c6b39bbd81e669f48a2d3e4
BenderChrist <140438837+BenderChrist@users.noreply.github.com>
2023-08-14 10:35:45 -0700

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,11 @@
+function showmenu()
+{
+if(document.getElementById("menu").style.display=="block")
+{
+document.getElementById("menu").style.display="none"
+}
+else
+{
+document.getElementById("menu").style.display="block"
+}
+}
